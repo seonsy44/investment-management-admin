9ed787d1a2e186192ef9faedd2f1df271a50d152
seon <seonsy44@gmail.com>
2022-11-15 00:34:06 +0900

    feat: getAxios

---
+++ src/utils/getAxios.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse, AxiosError } from 'axios';
+import LocalToken from '@repositories/LocalTokenRepository';
+
+const DEFAULT_CONFIG = {
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+const instance = axios.create(DEFAULT_CONFIG);
+const bearerInstance = axios.create(DEFAULT_CONFIG);
+
+instance.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error: AxiosError) => error.response
+);
+
+bearerInstance.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error: AxiosError) => error.response
+);
+
+const getAxios = ({ bearer }: { bearer: boolean }) => {
+  if (!bearer) return instance;
+
+  bearerInstance.defaults.headers.common.Authorization = `Bearer ${LocalToken.get()}`;
+  return bearerInstance;
+};
+
+export default getAxios;
