d59cad4a9085159028d9891cb5711c1309f17aa9
seon <seonsy44@gmail.com>
2022-11-15 15:49:43 +0900

    feat: Footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,15 @@
+import { flexBox } from '@styles/mixins';
+import styled from 'styled-components';
+
+function Footer() {
+  return <Container>Copyright © December and Company Inc. </Container>;
+}
+
+export default Footer;
+
+const Container = styled.div`
+  ${flexBox()}
+  width: 100%;
+  height: 100px;
+  background-color: ${({ theme }) => theme.GRAY_DARK};
+`;
