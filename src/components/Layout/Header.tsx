ec548da5f5507bef1079546957e51d6098d783f5
seon <seonsy44@gmail.com>
2022-11-15 15:38:49 +0900

    feat: Header

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,47 @@
+import { flexBox } from '@styles/mixins';
+import styled from 'styled-components';
+import { HiOutlineMenuAlt1, HiOutlineQuestionMarkCircle, HiOutlineBell } from 'react-icons/hi';
+import { useRouter } from 'next/router';
+import { headerTitle } from '@utils/const';
+
+function Header() {
+  const { pathname } = useRouter();
+
+  return (
+    <Container>
+      <Side>
+        <HiOutlineMenuAlt1 />
+        <span>{headerTitle[pathname]}</span>
+      </Side>
+
+      <Side>
+        <HiOutlineQuestionMarkCircle />
+        <HiOutlineBell />
+      </Side>
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  ${flexBox('row', 'space-between')};
+  grid-area: header;
+  padding: 0 20px;
+  background-color: white;
+  box-shadow: 0 1px 3px ${({ theme }) => theme.GRAY_DARK};
+  z-index: 2;
+`;
+
+const Side = styled.div`
+  ${flexBox()}
+  font-size: 18px;
+
+  > svg {
+    font-size: 22px;
+  }
+
+  > svg:first-child {
+    margin-right: 10px;
+  }
+`;
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,8 +1,27 @@
 import { HiOutlineTemplate, HiOutlineTable, HiOutlineUsers, HiOutlineLogout } from 'react-icons/hi';
 
+const contents = {
+  dashboard: { name: '대시보드', href: '/' },
+  accounts: { name: '계좌 목록', href: '/accounts' },
+  users: { name: '사용자 목록', href: '/users' },
+  logout: { name: '로그아웃', href: null },
+};
+
 export const sider = [
-  { id: 1, name: '대시보드', keyword: 'dashbaord', href: '/', Icon: HiOutlineTemplate },
-  { id: 2, name: '계좌 목록', keyword: 'accounts', href: '/accounts', Icon: HiOutlineTable },
-  { id: 3, name: '사용자 목록', keyword: 'users', href: '/users', Icon: HiOutlineUsers },
-  { id: 9999, name: '로그아웃', keyword: 'logout', Icon: HiOutlineLogout },
+  {
+    id: 1,
+    name: contents.dashboard.name,
+    keyword: 'dashbaord',
+    href: contents.dashboard.href,
+    Icon: HiOutlineTemplate,
+  },
+  { id: 2, name: contents.accounts.name, keyword: 'accounts', href: contents.accounts.href, Icon: HiOutlineTable },
+  { id: 3, name: contents.users.name, keyword: 'users', href: contents.users.href, Icon: HiOutlineUsers },
+  { id: 9999, name: contents.logout.name, keyword: 'logout', href: contents.logout.href, Icon: HiOutlineLogout },
 ];
+
+export const headerTitle = {
+  [contents.dashboard.href]: contents.dashboard.name,
+  [contents.accounts.href]: contents.accounts.name,
+  [contents.users.href]: contents.users.name,
+};
