88c29532b2ac779c14ed5a1fc86842f658ffc3fc
seon <seonsy44@gmail.com>
2022-12-03 18:56:02 +0900

    modify: server data 수정

--- server/generateData.ts
+++ server/generateData.ts
@@ -16,11 +16,21 @@ for (let i = 1; i < 101; i++) {
   // generate fake users and settings
   const id = faker.datatype.number()
   const uuid = faker.datatype.uuid()
+  const allow_marketing_push = faker.datatype.boolean()
+  const is_active = faker.datatype.boolean()
+  const is_staff = faker.datatype.boolean()
+  const name = faker.name.fullName()
+  const account_count = faker.datatype.number({ min: 2, max: 11 })
+
   const user = {
     id: i,
     uuid,
+    allow_marketing_push,
+    is_active,
+    is_staff,
+    name,
+    account_count,
     photo: faker.internet.avatar(),
-    name: faker.name.fullName(),
     email: faker.internet.email(),
     age: faker.datatype.number({ min: 20, max: 66 }),
     gender_origin: faker.datatype.number({ min: 1, max: 4 }),
@@ -35,10 +45,10 @@ for (let i = 1; i < 101; i++) {
   const setting = {
     id,
     uuid,
-    allow_marketing_push: faker.datatype.boolean(),
+    allow_marketing_push,
+    is_active,
+    is_staff,
     allow_invest_push: faker.datatype.boolean(),
-    is_active: faker.datatype.boolean(),
-    is_staff: faker.datatype.boolean(),
     created_at: faker.date.between('2019-04-01', '2022-08-01'),
     updated_at: faker.date.between('2019-04-01', '2022-08-01'),
   }
@@ -46,12 +56,13 @@ for (let i = 1; i < 101; i++) {
   userSetting.push(setting)
 
   // generate fake accounts
-  for (let j = 1; j < faker.datatype.number({ min: 2, max: 11 }); j++) {
+  for (let j = 1; j < account_count; j++) {
     const accountBrokerCode = brokercode.sort(() => 0.5 - Math.random())[0]
     const status = accountStatusCode.sort(() => 0.5 - Math.random())[0]
     const account = {
       id: j,
-      user_id: i,
+      user_id: uuid,
+      user_name: name,
       uuid: faker.datatype.uuid(),
       broker_id: accountBrokerCode,
       status,
--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ const auth = require('json-server-auth')
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
 
+router.db._.id = 'uuid'
 app.db = router.db
 const rules = auth.rewriter({
   users: 660,
@@ -16,6 +17,6 @@ app.use(rules)
 app.use(auth)
 app.use(router)
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log('JSON Server is running...')
 })
